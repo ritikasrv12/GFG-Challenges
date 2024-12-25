dbd5725468c2902120a2701770f66257bcca2686
Ritika Srivastava <105850960+ritikasrv12@users.noreply.github.com>
2024-12-25 18:09:38 +0530

    Set Matrix Zeroes.cpp

---
+++ Easy/Set Matrix Zeroes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    void setMatrixZeroes(vector<vector<int>> &mat) {
+        int n = mat.size(), m = mat[0].size(), col0 = 1;
+        for (int i = 0; i < n; ++i) {
+            if (mat[i][0] == 0) col0 = 0;
+            for (int j = 1; j < m; ++j)
+                if (mat[i][j] == 0)
+                    mat[i][0] = mat[0][j] = 0;
+        }
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = m - 1; j >= 1; --j)
+                if (mat[i][0] == 0 || mat[0][j] == 0)
+                    mat[i][j] = 0;
+            if (col0 == 0) mat[i][0] = 0;
+        }
+        // code here
+    }
+};
