5a3925c032878f320c7cd3c4aa24e0d0e19f71c3
Ritika Srivastava <105850960+ritikasrv12@users.noreply.github.com>
2024-12-27 17:22:18 +0530

    Count pairs with given sum.cpp

---
+++ Easy/Count pairs with given sum.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    int countPairs(vector<int> &arr, int target) {
+        unordered_map<int, int> freq;
+        int count = 0;
+        for (int num : arr) 
+            count += freq[target - num], freq[num]++;
+        return count;
+        // Code here
+    }
+};
