f108bd6f6738c8c887f26a415728f062f2650f1e
Ritika Srivastava <105850960+ritikasrv12@users.noreply.github.com>
2025-01-02 19:47:33 +0530

    Subarrays with sum K.cpp

---
+++ Easy/Subarrays with sum K.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    int countSubarrays(vector<int> &arr, int k) {
+        unordered_map<int, int> prefixSumCount = {{0, 1}};
+        int sum = 0, count = 0;
+
+        for (int num : arr) {
+            sum += num;
+            count += prefixSumCount[sum - k];
+            prefixSumCount[sum]++;
+        }
+
+        return count;
+        // code here
+    }
+};
