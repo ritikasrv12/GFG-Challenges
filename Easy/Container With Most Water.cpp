52cb27d8d201614d5aab7b96a41a6f9464c4806d
Ritika Srivastava <105850960+ritikasrv12@users.noreply.github.com>
2025-01-13 21:30:59 +0530

    Container With Most Water.cpp

---
+++ Easy/Container With Most Water.cpp
@@ -0,0 +1,10 @@
+class Solution {
+
+  public:
+    int maxWater(vector<int> &arr) {
+        int l = 0, r = arr.size() - 1, res = 0;
+        while (l < r) res = max(res, (r - l) * (arr[l] < arr[r] ? arr[l++] : arr[r--]));
+        return res;
+        // code here
+    }
+};
