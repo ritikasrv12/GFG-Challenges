812ba52d59226b1d21a6695f279c7f62e6f7e6fb
Ritika Srivastava <105850960+ritikasrv12@users.noreply.github.com>
2025-01-15 19:07:32 +0530

    Longest Subarray with Sum K.cpp

---
+++ Easy/Longest Subarray with Sum K.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& arr, int k) {
+        unordered_map<int, int> mp;
+        int sum = 0, res = 0;
+        for (int i = 0; i < arr.size(); ++i) {
+            sum += arr[i];
+            if (sum == k) res = i + 1;
+            if (mp.count(sum - k)) res = max(res, i - mp[sum - k]);
+            if (!mp.count(sum)) mp[sum] = i;
+        }
+        return res;
+    }
+};
