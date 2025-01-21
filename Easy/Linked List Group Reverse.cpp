f30d089e56d4a85cafcde2cd9930dfbdd2c581f9
Ritika Srivastava <105850960+ritikasrv12@users.noreply.github.com>
2025-01-21 16:45:44 +0530

    Linked List Group Reverse.cpp

---
+++ Easy/Linked List Group Reverse.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    Node* reverseKGroup(Node* head, int k) {
+        if (!head || k <= 1) return head;
+
+        Node* curr = head;
+        Node* newHead = nullptr;
+        Node* tail = nullptr;
+
+        while (curr) {
+            Node* groupPrev = nullptr;
+            Node* groupCurr = curr;
+            int count = 0;
+
+            while (curr && count < k) {
+                Node* next = curr->next;
+                curr->next = groupPrev;
+                groupPrev = curr;
+                curr = next;
+                count++;
+            }
+
+            if (!newHead) newHead = groupPrev;
+            if (tail) tail->next = groupPrev;
+            tail = groupCurr;
+        }
+
+        return newHead;
+    }
+};
