ad343acd07c89766b3159139692e1eeb7337dc93
Ritika Srivastava <105850960+ritikasrv12@users.noreply.github.com>
2025-01-26 16:12:40 +0530

    Remove loop in Linked List.cpp

---
+++ Easy/Remove loop in Linked List.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void removeLoop(Node* head) {
+        Node *slow = head, *fast = head;
+        while (fast && fast->next && (slow = slow->next) != (fast = fast->next->next));
+        if (!fast || !fast->next) return;
+        for (slow = head; slow != fast; slow = slow->next, fast = fast->next);
+        while (fast->next != slow) fast = fast->next;
+        fast->next = nullptr;
+    }
+};
