8ae262549c485a66b03a09ea7e35ec3a5735a44c
Ritika Srivastava <105850960+ritikasrv12@users.noreply.github.com>
2025-02-15 22:09:32 +0530

    Lowest Common Ancestor in a BST.cpp

---
+++ Easy/Lowest Common Ancestor in a BST.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    Node* LCA(Node* root, Node* n1, Node* n2) {
+        while (root && (root->data > max(n1->data, n2->data) || root->data < min(n1->data, n2->data)))
+            root = (root->data > n1->data) ? root->left : root->right;
+        return root;
+    }
+};
