7e629327bb6a90e398bfb3a58f9377e4c9d62f2e
Ritika Srivastava <105850960+ritikasrv12@users.noreply.github.com>
2025-02-23 12:28:22 +0530

    Next Greater Element.cpp

---
+++ Easy/Next Greater Element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> nextLargerElement(vector<int>& arr) {
+        stack<int> s;
+        for (int i = arr.size() - 1; i >= 0; i--) {
+            int val = arr[i];
+            while (!s.empty() && s.top() <= val) s.pop();
+            arr[i] = s.empty() ? -1 : s.top();
+            s.push(val);
+        }
+        return arr;
+    }
+};
