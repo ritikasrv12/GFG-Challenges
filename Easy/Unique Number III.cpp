db5db10a2767e0708391e0f49a9c3dac490cff47
Ritika Srivastava <ritikasrivastava646@gmail.com>
2025-04-24 21:12:59 +0530

    Unique Number III.cpp

---
+++ Easy/Unique Number III.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    int getSingle(vector<int> &arr) {
+        int ones = 0, twos = 0;
+        for (int num : arr) {
+            ones = (ones ^ num) & ~twos;
+            twos = (twos ^ num) & ~ones;
+        }
+        return ones;
+    }
+};
